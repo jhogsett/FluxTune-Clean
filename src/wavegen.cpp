4290f3ceae28b60fd2d77f35168b44dab3539c68
Jerry Hogsett <jhogsett@gmail.com>
2025-07-18 14:04:24 -0700

    optimization causes interference without being considered in using code

--- src/wavegen.cpp
+++ src/wavegen.cpp
@@ -9,20 +9,20 @@ WaveGen::WaveGen(MD_AD9833 * sig_gen)
 }
 
 void WaveGen::set_frequency(float frequency, bool main){
-	if(frequency != _frequency || _main != main){
+	// if(frequency != _frequency || _main != main){
 		// Serial.println("set freq");
 		_sig_gen->setFrequency((MD_AD9833::channel_t)(main ? 0 : 1), frequency);
-		_frequency = frequency;
-		_main = main;
-	}
+		// _frequency = frequency;
+		// _main = main;
+	// }
 }
 
 void WaveGen::set_active_frequency(bool main){
-	if(_main != main){
+	// if(_main != main){
 		// Serial.println("set act freq");
 		_sig_gen->setActiveFrequency((MD_AD9833::channel_t)(main ? 0 : 1));
-		_main = main;
-	}
+		// _main = main;
+	// }
 }
 
 void WaveGen::force_refresh(){
